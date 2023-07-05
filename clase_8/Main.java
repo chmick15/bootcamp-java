13a1c36a8d7c4e7733476e709dbcb99722033cc8
Nicolas Debray <97224385+chmick15@users.noreply.github.com>
2023-07-05 09:19:59 +0200

    generate password

--- clase_8/Main.java
+++ clase_8/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println(saludar());
         calcularAreaRectangulo(10, 402);
         calcularAreaTriangulo(24,12);
+        System.out.println(generatePassword());
     }
 
     private static String saludar(){
@@ -19,5 +20,15 @@ public class Main {
     private static void calcularAreaTriangulo(double base, double height){
         double result = (base * height)/2;
         System.out.println("El area del triangulo es de: " + result);
+    }
+
+    private static String generatePassword(){
+        String password = "Nw82nf";
+        double firstAleatorio = Math.random() * 1000;
+        long firstNumAleatorio = Math.round(firstAleatorio);
+        double secondAleatorio = Math.random() * 1000;
+        long secondNumAleatorio = Math.round(secondAleatorio);
+        String nuevoPassword = firstNumAleatorio + password + secondNumAleatorio;
+        return nuevoPassword;
     }
 }
